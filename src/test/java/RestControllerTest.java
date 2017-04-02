26872246a87b1850b4db83c93c6363b82c086a6a
Daniele Mazzotti <kappei84@gmail.com>
2017-04-02 18:41:07 +0200

    added GET Rest Unit Test; fixed log4j properties

--- src/test/java/RestControllerTest.java
+++ src/test/java/RestControllerTest.java
@@ -1,4 +1,6 @@
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.mockito.Mockito.when;
 
 import java.time.LocalDate;
 
@@ -21,72 +23,59 @@ import com.daniele.hibernate.rest.UserController;
 import com.daniele.hibernate.service.UserService;
 import com.daniele.hibernate.shared.JsonUtils;
 
-
 @RunWith(MockitoJUnitRunner.class)
 public class RestControllerTest {
-	
-	private MockMvc mockMvc;
-	
-	@Mock
-	UserService userDetailsService;
-	
-	// Inside the UserResource the bean UserDetailsService is substituted with the bean annotated @Mock within this class
+
+    private MockMvc mockMvc;
+    
+    private static UserAccount user;
+
+    @Mock
+    UserService userDetailsService;
+
+    // Inside the UserResource the bean UserDetailsService is substituted with the bean annotated @Mock within this class
     @InjectMocks
     private UserController userResource;
-	
+
     @Before
     public void setUp() {
-    	mockMvc = MockMvcBuilders.standaloneSetup(userResource).build();
-		MockitoAnnotations.initMocks(this);
+        mockMvc = MockMvcBuilders.standaloneSetup(userResource).build();
+        user = getDummyUser(1);
+        MockitoAnnotations.initMocks(this);
     }
-    
+
     @Test
     public void testMockitoInjection() {
-		Assert.assertTrue(userDetailsService != null);
+        Assert.assertTrue(userDetailsService != null);
     }
-    
+
     @Test
     public void readUserDetails() throws Exception {
-        /*
-    	int detailId = 1;
-    	Mockito.when(userDetailsService.getUserById(1)).thenReturn(userDetails);
-    	mockMvc.perform(MockMvcRequestBuilders.get("/rest/" + detailId))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(contentType))
-                .andExpect(jsonPath("$", hasSize(2)))
-                .andExpect(jsonPath("$[0].id", is(this.bookmarkList.get(0).getId().intValue())))
-                .andExpect(jsonPath("$[0].uri", is("http://bookmark.com/1/" + userName)))
-        );
-        */
-    	System.out.println("TODO: readUserDetails()");
+        Assert.assertNotNull(user);
+    	when(userDetailsService.getUserById(1)).thenReturn(user);
+        mockMvc.perform(MockMvcRequestBuilders.get("/{id}", 1))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE));
     }
-    
+
     @Test
     public void testCreationOfANewUser() throws Exception {
-    	UserAccount user = getDummyUser(1);
+    	Assert.assertNotNull(user);
     	String userDetailsAsString = JsonUtils.prettyJsonString(user);
-    	
+
         mockMvc.perform(MockMvcRequestBuilders.post("/save")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(userDetailsAsString))
-                .andExpect(status().isOk());
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(userDetailsAsString))
+            .andExpect(status().isOk());
     }
-    
+
     private UserAccount getDummyUser(int i) {
-    	Address address = new Address.AddressBuilder()
-				.withCity("City " + i)
-				.withStreet("Dummy Street")
-				.withStreetNumber((short )i)
-				.withZipCode("9009" + i)
-				.build();
-				
-		UserAccount user = new UserAccount.UserBuilder()
-				.withName("User" + i)
-				.withDescription("Description field " + i)
-				.withAddress(address)
-				.withJoinDate(LocalDate.now())
-				.build();
-		
-		return user;
+        Address address = new Address.AddressBuilder().withCity("City " + i).withStreet("Dummy Street")
+                .withStreetNumber((short) i).withZipCode("9009" + i).build();
+
+        UserAccount user = new UserAccount.UserBuilder().withName("User" + i).withDescription("Description field " + i)
+                .withAddress(address).withJoinDate(LocalDate.now()).build();
+
+        return user;
     }
 }
